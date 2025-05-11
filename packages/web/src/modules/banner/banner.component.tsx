9a7a125bcb4b9f9c7f3be41308b150ec2ab1c649
mrustamov1 <mr.rusttamov@gamil.com>
2025-05-12 00:04:36 +0500

    small fix

--- packages/web/src/modules/banner/banner.component.tsx
+++ packages/web/src/modules/banner/banner.component.tsx
@@ -9,14 +9,13 @@ export function Banner() {
       <main
         style={{
           backgroundImage: `url(${Images.banner})`,
-          backgroundSize: "cover",
         }}
-        className="relative h-[100vh] flex justify-end flex-col"
+        className="relative h-screen flex flex-col justify-end bg-cover bg-center"
       >
-        <div className="flex-col flex  ps-[60px] pb-[60px]">
-          <div className="text-[#fff] flex-col flex items-start leading-[70px] pb-[30px]">
+        <div className="flex-col flex ps-[60px] pb-[60px]  max-md:ps-[30px] max-md:pb-[30px]">
+          <div className="text-[#fff] flex-col flex items-start leading-[70px] max-md:leading-[50px] pb-[30px]">
             <span className="text-2xl">NEW</span>
-            <span className="text-[80px]">PowerBeats Pro 2</span>
+            <span className="text-[80px] max-sm:text-[40px]">PowerBeats Pro 2</span>
             <span className="text-[32px]">Built for athletes</span>
           </div>
           <div>
--- packages/web/src/modules/popular-products/popular-products.component.tsx
+++ packages/web/src/modules/popular-products/popular-products.component.tsx
@@ -21,18 +21,20 @@ export function PopularProducts() {
   return (
     <section className="flex flex-col justify-center items-center py-[4.5rem]">
       <h1 className="text-[2.5rem] font-bold">Popular Now</h1>
-      <div className="w-full max-w-[1200px] mx-auto pt-16">
-        <div className="flex gap-5 perspective-[1000px]">
+      <div className="w-full max-w-[1200px] mx-auto pt-16 max-md:max-w-full p-4">
+        <div className="flex gap-5 overflow-x-auto md:overflow-x-visible scroll-smooth scrollbar-hidden">
           {products.slice(0, 3).map((product) => (
             <article
               onClick={() => handleID(product.id)}
               key={product.id}
-              className="cursor-pointer bg-[#fff] p-[1.875rem] rounded-2xl flex flex-col leading-8 flex-1 shadow-[2px_4px_12px_rgba(0,0,0,0.1)] transition-transform duration-300 hover:scale-[1.02]"
+              className="max-md:min-w-[400px] cursor-pointer bg-[#fff] p-[1.875rem] rounded-2xl flex flex-col leading-8 w-full shadow-[2px_4px_12px_rgba(0,0,0,0.1)] transition-transform duration-300 hover:scale-[1.02]"
             >
               <span className="text-[#e31837] font-bold">
                 {product.version}
               </span>
-              <span className="font-bold text-[32px]">{product.model}</span>
+              <span className="font-bold text-[32px] max-xl:text-[28px] max-lg:text-[20px] max-md:text-[32px]">
+                {product.model}
+              </span>
               <span className="font-bold">{product.price}</span>
               <img
                 className="max-h-[295px] my-auto"
@@ -57,20 +59,24 @@ export function PopularProducts() {
       {/* VIDEO */}
       {/* --------------------------------------------------------------------------- */}
 
-      <div className="relative w-full h-auto">
+      <div className="relative w-full h-screen">
         <video
           src={video}
           autoPlay
           loop
           muted
           playsInline
-          className="w-full h-auto shadow-lg mt-20 relative"
+          className="w-full h-full object-cover shadow-lg"
         ></video>
-        <div className="flex-col flex ps-[60px] pb-[60px] absolute top-[60%]">
-          <div className="text-[#fff] flex-col flex items-start leading-[70px] pb-[30px]">
+        <div className="absolute left-[60px] bottom-[60px] flex flex-col max-md:left-[30px] max-md:bottom-[30px]">
+          <div className="text-[#fff] flex-col flex items-start leading-[70px] max-sm:leading-[50px] pb-[30px] max-sm:pb-[20px]">
             <span className="text-2xl">NEW</span>
-            <span className="text-[80px] max-w-[100px]">Beats Cables</span>
-            <span className="text-[32px]">Fresh juice for your devices</span>
+            <span className="text-[80px] max-w-[100px] max-sm:text-[60px]">
+              Beats Cables
+            </span>
+            <span className="text-[32px] max-sm:text-[25px]">
+              Fresh juice for your devices
+            </span>
           </div>
           <div className="flex gap-5">
             <Button
@@ -78,7 +84,7 @@ export function PopularProducts() {
               title="SHOP"
               mode="primary"
             />
-            <Button title=" WATCH THE FILM" mode="light" />
+            <Button title="WATCH THE FILM" mode="light" />
           </div>
         </div>
       </div>
