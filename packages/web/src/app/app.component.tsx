3062ec2188a085f9ec79cd2a81d7d6776a367847
mrustamov1 <mr.rusttamov@gamil.com>
2025-05-20 22:21:17 +0500

    small fix

--- packages/web/src/app/app.component.tsx
+++ packages/web/src/app/app.component.tsx
@@ -10,7 +10,7 @@ export function App() {
     <AuthorizationProvider>
       <ModalProvider>
         <QueryClientProvider client={querClient}>
-          {/* <MainRoutes /> */}
+          <MainRoutes />
         </QueryClientProvider>
       </ModalProvider>
     </AuthorizationProvider>
