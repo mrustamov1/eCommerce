edc0e0ccbcd49ec839174606266179358d46d6a2
mrustamov1 <mr.rusttamov@gamil.com>
2025-05-22 22:27:32 +0500

    validate with zod

--- packages/server/src/models/product.model.ts
+++ packages/server/src/models/product.model.ts
@@ -1,8 +1,12 @@
-export type ProductModel = {
-  id: string;
-  verstion: string;
-  title: string;
-  subTitle: string;
-  price: string;
-  colors: string[];
-};
+import { z } from "zod";
+
+export const ProductModelSchema = z.object({
+  id: z.string(),
+  verstion: z.string(),
+  title: z.string(),
+  subTitle: z.string(),
+  price: z.string(),
+  colors: z.string().array()
+})
+
+export type ProductModel = z.infer<typeof ProductModelSchema>
